c3bef5dc6aa9695670dd44aa852065b75bcb9211
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2020-09-15 01:34:02 -0300

    criação das rotas

--- src/app.js
+++ src/app.js
@@ -9,25 +9,69 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (req, res) => {
-    res.json("Ok !");
+    const {title} = req.query;
+    const results = title
+        ? repositories.filter(rep => rep.title.includes(title))
+        : repositories;    
+    
+    return res.json(results);
 });
 
-app.post("/repositoires", (req, res) => {
-    res.json("Ok !");
+app.post("/repositories", (req, res) => {
+    const {title, url, techs} = req.body;
+    const repos = {id:uuid(), title, url, techs, like: 0};
+    repositories.push(repos);
+
+    return res.json(repos);
 });
 
 app.post("/repositories/:id/like", (req, res) => {
-    res.json("Ok !");
+    const {id} = req.params;
+    const reposIndex = repositories.find(repos => repos.id == id);
+
+    if (reposIndex < 0){
+        return res.status(400).json({error: "Repos not found !"});
+    }
+
+    console.log(reposIndex);
+
+    reposIndex.like += 1;
+    //repositories[reposIndex].like += 1;
+    
+    return res.send(`${reposIndex.title} | ${reposIndex.like}`);
 })
 
 app.put("/repositories/:id", (req, res) => {
-    res.json("Ok !");
+    const {id} = req.params;
+    const {title, url, techs} = req.body;
+
+    const reposIndex = repositories.findIndex(repos => repos.id == id);
+
+    if (reposIndex < 0){
+        return res.status(400).json({error: "Repos not found !"});
+    }
+
+    const repos = {id, title, url, techs};
+    repositories[reposIndex] = repos;
+
+    return res.json(repos);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-    res.json("Ok !");
+    const {id} = req.params;
+
+    const reposIndex = repositories.findIndex(repos => repos.id == id);
+    if (reposIndex < 0){
+        return res.status(400).json({error: "Repos not found !"});
+    }
+    repositories.splice(reposIndex,1);
+
+    console.log("Removed !");
+    return res.status(202).send("Repos removed !");
 })
 
 app.listen(3334, () => {
     console.log("Sucess !");
 })
+
+module.exports = app;
